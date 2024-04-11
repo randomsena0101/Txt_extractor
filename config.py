fa126dd4439bf972d7bb8634a4f8a6630429a657
randomsena0101 <166689915+randomsena0101@users.noreply.github.com>
2024-04-11 19:22:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6991599062:AAFwn6ubznUzL51Wi5jH0EaODVyck3w4atQ")
-    API_ID = int(os.environ.get("API_ID", "27862677"))
-    API_HASH = os.environ.get("API_HASH", "e343ce2c81b2b6c2c0d6bee58284e3bd")
-    AUTH_USERS = os.environ.get("AUTH_USERS", "5925499677")
+    BOT_TOKEN = os.environ.get("6991599062:AAFwn6ubznUzL51Wi5jH0EaODVyck3w4atQ")
+    API_ID = int(os.environ.get("27862677")
+    API_HASH = os.environ.get("e343ce2c81b2b6c2c0d6bee58284e3bd")
+    AUTH_USERS = os.environ.get("5925499677")
